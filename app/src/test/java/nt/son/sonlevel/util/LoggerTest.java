c4e58ec7d7196de39641760b0508156e83576f94
Son <thanhsontube@gmail.com>
2017-09-21 15:32:48 +0800

    Add mockito and Junit.

---
+++ app/src/test/java/nt/son/sonlevel/util/LoggerTest.java
@@ -0,0 +1,14 @@
+package nt.son.sonlevel.util;
+
+import junit.framework.Assert;
+
+/**
+ * Created by sonnt on 9/21/17.
+ */
+public class LoggerTest {
+    @org.junit.Test
+    public void t1() {
+        Assert.assertEquals(1,1);
+    }
+
+}
